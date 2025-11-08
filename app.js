83ee7a1f6be9f8d4ad42c87a0b7e6b8b96ab20f4
jagg26 <john.ale26@gmail.com>
2025-11-08 14:08:29 -0300

    Initital checkin

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+app.use('/', require('./routes'));
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
+module.exports = app;
